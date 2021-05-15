52548415b1ad8aa10e36deb819fafa377439a2af
kaustubh <kaustubhharapanahalli@gmail.com>
2021-05-15 16:32:29 +0530

    Create Models class to create WordsDictionary database table

--- backend/learn_words/words/models.py
+++ backend/learn_words/words/models.py
@@ -1,4 +1,31 @@
 """Define models (database tables) for storing words."""
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class WordsDictionary(models.Model):
+    """WordsDictionary: Database table to save words in groups."""
+
+    group = models.CharField(max_length=10, default=None)
+    word = models.CharField(max_length=50, unique=True, blank=False, null=False)
+    definition = models.JSONField()
+    example_sentences = models.JSONField(default=dict)
+    gre_synonyms = models.JSONField()
+    synonyms = models.JSONField()
+    antonyms = models.JSONField()
+
+    def __str__(self) -> str:
+        """
+        __str__: String representation of each entry.
+
+        Returns
+        -------
+        str
+            Word name for the row.
+        """
+        return self.word
+
+    class Meta:
+        """Meta class to define Verbose names of the Model."""
+
+        verbose_name = "Words Dictionary"
+        verbose_name_plural = "Words Dictionary"
