d990c7f90ac47960df22288ae509ab4cd69d9622
Kaustubh M Harapanahalli <22560434+kaustubhharapanahalli@users.noreply.github.com>
2021-05-17 21:41:33 +0530

    Update testing.py

--- backend/learn_words/learn_words/settings/testing.py
+++ backend/learn_words/learn_words/settings/testing.py
@@ -1,10 +1,5 @@
 """Settings file for development of application."""
-import os
-from pathlib import Path
-
 from .base import *
-
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 DEBUG = True
 
@@ -12,7 +7,11 @@ DEBUG = True
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "learn_words",
+        "USER": "postgres",
+        "PASSWORD": "password",
+        "HOST": "postgres",
+        "PORT": "5432",
     }
 }
