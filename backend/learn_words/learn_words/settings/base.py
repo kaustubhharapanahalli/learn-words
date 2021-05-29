653f9d85883704c1df797495280055209164f98a
kaustubh <kaustubhharapanahalli@gmail.com>
2021-05-29 23:23:56 +0530

    Update templates path and add staticdirs list containing paths of static files

--- backend/learn_words/learn_words/settings/base.py
+++ backend/learn_words/learn_words/settings/base.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = "learn_words.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -109,6 +109,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
