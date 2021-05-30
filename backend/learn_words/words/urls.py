be5363c6ab633414d316a2b9b0f3eda6a65b58b2
kaustubh <kaustubhharapanahalli@gmail.com>
2021-05-30 10:51:55 +0530

    Add a URL endpoint to allow request calls for WordsDictionaryAPI view function

--- backend/learn_words/words/urls.py
+++ backend/learn_words/words/urls.py
@@ -1,7 +1,8 @@
 """URLs to communicate with the APIs of the words app."""
 from django.urls import path
-from words.views import post_words
+from words.views import post_words, WordsDictionaryAPI
 
 urlpatterns = [
+    path("", WordsDictionaryAPI.as_view()),
     path("add_words/", post_words),
 ]
