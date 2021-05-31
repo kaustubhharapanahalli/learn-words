236d0b8f69980af2a46724612c1e7c7633b528ac
kaustubh <kaustubhharapanahalli@gmail.com>
2021-05-31 21:45:59 +0530

    Define a home page component

---
+++ frontend/learn_words_ui/src/components/Home.js
@@ -0,0 +1,41 @@
+import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    minHeight: "100vh",
+  },
+  main: {
+    marginTop: theme.spacing(8),
+    marginBottom: theme.spacing(2),
+  },
+  title: {
+    backgroundColor: "black",
+    color: "white",
+  },
+}));
+
+export default function Home() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <Container component="main" className={classes.main} maxWidth="md">
+        <Typography
+          variant="h1"
+          component="h1"
+          gutterBottom
+          className={classes.title}
+        >
+          Learn Words
+        </Typography>
+      </Container>
+    </div>
+  );
+}
