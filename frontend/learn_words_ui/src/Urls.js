dbaa71ae2ad128445f6877a689d8079d41342cf7
kaustubh <kaustubhharapanahalli@gmail.com>
2021-05-31 21:45:41 +0530

    Define Urls for rendering different components

---
+++ frontend/learn_words_ui/src/Urls.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import Home from "./components/Home";
+
+function Urls(props) {
+  return (
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home {...props} />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default Urls;
